932db27b72c63a6f0d070a35ac103cbe1e406d74
gaulbaram0111 <gaulbaram0111@gmail.com>
2023-07-05 15:22:46 +0900

    230705

--- chapter9/src/App.js
+++ chapter9/src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useMemo, useReducer, useRef } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import TodoEditor from "./components/TodoEditor";
+import TodoList from "./components/TodoList";
+
+export const TodoStateContext = React.createContext();
+export const TodoDispatchContext = React.createContext();
 
 function App() {
+  const today = new Date().toDateString();
+  // 테스트용 목업 데이터
+  const mockTodo = [
+    {
+      index: 0,
+      isDone: false,
+      text: "test",
+      date: today,
+    },
+    {
+      index: 1,
+      isDone: false,
+      text: "test2",
+      date: today,
+    },
+  ];
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "CREATE":
+        return [action.newItem, ...state];
+      case "UPDATE":
+        return state.map((it) =>
+          it.index == action.targetId ? { ...it, isDone: !it.isDone } : it
+        );
+      case "DELETE":
+        return state.filter((it) => it.index !== action.targetId);
+      default:
+        return state;
+    }
+  };
+
+  const [todo, dispatch] = useReducer(reducer, mockTodo);
+  const indexRef = useRef(2);
+
+  const addTodo = (text) => {
+    const newItem = {
+      index: indexRef.current,
+      isDone: false,
+      text,
+      date: new Date().toDateString(),
+    };
+
+    dispatch({ type: "CREATE", newItem: newItem });
+
+    indexRef.current += 1;
+  };
+
+  const onUpdate = useCallback((targetId) => {
+    dispatch({ type: "UPDATE", targetId: targetId });
+  }, []);
+
+  const onDelete = useCallback((targetId) => {
+    dispatch({ type: "DELETE", targetId: targetId });
+  }, []);
+
+  const memoizedDispatches = useMemo(() => {
+    return { addTodo, onUpdate, onDelete };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <TodoStateContext.Provider value={todo}>
+        <TodoDispatchContext.Provider value={memoizedDispatches}>
+          <TodoEditor />
+          <TodoList />
+        </TodoDispatchContext.Provider>
+      </TodoStateContext.Provider>
     </div>
   );
 }
---
+++ chapter9/src/components/TodoEditor.js
@@ -0,0 +1,43 @@
+import { useContext, useRef, useState } from "react";
+import "./TodoEditor.css";
+import { TodoDispatchContext } from "../App";
+
+const TodoEditor = () => {
+  const { addTodo } = useContext(TodoDispatchContext);
+  const [text, setText] = useState("");
+  const inputRef = useRef();
+  const onChangeContent = (e) => {
+    setText(e.target.value);
+  };
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+  const onSubmit = () => {
+    // 빈 칸 입력 방지
+    if (!text) {
+      inputRef.current.focus();
+      return;
+    }
+    addTodo(text);
+    setText("");
+  };
+  return (
+    <div className="TodoEditor">
+      <h4>새로운 Todo 작성하기</h4>
+      <div className="editor_wrapper">
+        <input
+          ref={inputRef}
+          placeholder="새로운 Todo..."
+          value={text}
+          onChange={onChangeContent}
+          onKeyDown={onKeyDown}
+        />
+        <button onClick={onSubmit}>추가</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoEditor;
