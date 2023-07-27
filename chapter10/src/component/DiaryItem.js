59b558a380ce470a331f8dcffd8c148740aeb273
hahaha1110 <gaulbaram0111@gmail.com>
2023-07-27 17:32:39 +0900

    chapter10

---
+++ chapter10/src/component/DiaryItem.js
@@ -0,0 +1,35 @@
+import "./DiaryItem.css";
+import { useNavigate } from "react-router-dom";
+import { getEmotionImgById } from "../util";
+import Button from "./Button";
+import React from "react";
+
+const DiaryItem = ({ id, emotionId, content, date }) => {
+  const navigate = useNavigate();
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+  return (
+    <div className="DiaryItem">
+      <div
+        onClick={goDetail}
+        className={["img_section", `img_section_${emotionId}`].join(" ")}
+      >
+        <img alt={`emotions${emotionId}`} src={getEmotionImgById(emotionId)} />
+      </div>
+      <div onClick={goDetail} className="info_section">
+        <div className="date_wrapper">
+          {new Date(parseInt(date)).toLocaleString()}
+        </div>
+        <div className="content_wrapper">{content.slice(0, 25)}</div>
+      </div>
+      <div className="button_section">
+        <Button text={"수정하기"} onClick={goEdit} />
+      </div>
+    </div>
+  );
+};
+export default React.memo(DiaryItem);
---
+++ chapter10/src/pages/Edit.js
@@ -0,0 +1,53 @@
+import { useNavigate, useParams } from "react-router-dom";
+import useDiary from "../hooks/useDiary";
+import Header from "../component/Header";
+import Button from "../component/Button";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+import Editor from "../component/Editor";
+const Edit = () => {
+  const { id } = useParams();
+  const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
+  const data = useDiary(id);
+  const negative = useNavigate();
+  const goBack = () => {
+    negative(-1);
+  };
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      onDelete(id);
+      negative("/", { replace: true });
+    }
+  };
+
+  const onSubmit = (data) => {
+    if (window.confirm("일기를 정말 수정할까요?")) {
+      const { date, content, emotionId } = data;
+      onUpdate(id, date, content, emotionId);
+      negative("/", { replace: true });
+    }
+  };
+
+  if (!data) {
+    return <div>일기를 불러오고 있습니다...</div>;
+  } else {
+    return (
+      <div>
+        <Header
+          title={"일기 수정하기"}
+          leftChild={<Button text={"< 뒤로 가기"} onClick={goBack} />}
+          rightChild={
+            <Button
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={onClickDelete}
+            />
+          }
+        />
+        <Editor initData={data} onSubmit={onSubmit} />
+      </div>
+    );
+  }
+};
+export default Edit;
